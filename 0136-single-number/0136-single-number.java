6c72d52e0cf604d207096926a545461c53b60cfc
ankita-jugran <ankitajugran91@gmail.com>
2026-08-31 23:29:22 +0530

    Time: 1 ms (99.93%), Space: 46.8 MB (82.38%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -2,7 +2,9 @@ class Solution {
     public int singleNumber(int[] nums) {
         
         int result = 0;
-        for (int i = 0 ; i<nums.length ; i++){
+
+        for ( int i = 0 ; i < nums.length ; i ++ ){
+            //XOR operator
             result = result ^ nums[i];
         }
 
